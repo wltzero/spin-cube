b0d35e8c045d4178ddfbbcb8e1b151c7e5907123
wlt <1343539249@qq.com>
2025-04-22 14:50:11 +0800

    introduce ring_buffer lib

--- src/structs/ring_buffer.rs
+++ src/structs/ring_buffer.rs
@@ -2,35 +2,37 @@ use std::io;
 use std::io::Write;
 use crate::structs::screen::ScreenSize;
 use crate::structs::frame_buffer::FrameBuffer;
+use ringbuffer::{RingBuffer,AllocRingBuffer};
 
 pub struct RingBufferedRenderer {
-    buffers: Vec<FrameBuffer>,
-    current_index: usize,
+    buffers: AllocRingBuffer<FrameBuffer>,
     screen_size: ScreenSize,
+    current_index: usize,
 }
 
 impl RingBufferedRenderer {
     pub fn new(width: usize, height: usize, buffer_count: usize) -> Self {
-        let buffers = (0..buffer_count)
-            .map(|_| FrameBuffer::new(width, height))
-            .collect();
+        let mut buffers = AllocRingBuffer::new(buffer_count);
+        for _ in 0..buffer_count {
+            buffers.push(FrameBuffer::new(width, height));
+        }
 
         Self {
             buffers,
-            current_index: 0,
             screen_size: ScreenSize { width, height },
+            current_index: 0,
         }
     }
 
     pub fn current_buffer(&mut self) -> &mut FrameBuffer {
-        &mut self.buffers[self.current_index]
+        self.buffers.get_mut(self.current_index).unwrap()
     }
 
     pub fn next_buffer(&mut self) {
-        self.current_index = (self.current_index + 1) % self.buffers.len();
+        self.current_index += 1;
     }
 
     pub fn render(&self, stdout: &mut impl Write) -> io::Result<()> {
-        self.buffers[self.current_index].render(stdout, &self.screen_size)
+        self.buffers.get(self.current_index).unwrap().render(stdout, &self.screen_size)
     }
 }
